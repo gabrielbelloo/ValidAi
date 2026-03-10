c5d84aa8dc4a250571faccc20c4b3aadd568500a
gabrielbelloo <167886489+gabrielbelloo@users.noreply.github.com>
2026-03-10 09:20:43 -0300

    feat(icons): create icon components with SVGs
    create icon components and export index

---
+++ client/src/assets/icons/CheckIcon.jsx
@@ -0,0 +1,18 @@
+export default function CheckIcon() {
+    return (
+        <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill=""
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#66ff00"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 12.75 6 6 9-13.5"
+              />
+            </svg>
+    )
+}
---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,6 @@
+export { default as CheckIcon } from "./CheckIcon.jsx";
+export { default as XMarkIcon } from "./XMarkIcon.jsx";
+export { default as DownloadIcon } from "./DownloadIcon.jsx";
+export { default as PrinterIcon } from "./PrinterIcon.jsx";
+export { default as RefreshIcon } from "./RefreshIcon.jsx";
+export { default as UploadIcon } from "./UploadIcon.jsx"
